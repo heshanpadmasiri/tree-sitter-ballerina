b3d0e445b0fc4f0e1a572866039b3303bc69e703
Heshan Padmasiri <hpheshan@gmail.com>
2025-03-09 19:39:20 +0530

    Basic type

--- grammar.js
+++ grammar.js
@@ -11,7 +11,23 @@ module.exports = grammar({
   name: "tree_sitter_ballerina",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($._module_decl),
+    _module_decl: $ => choice(
+      $.type_decl
+    ),
+    type_decl: $ => seq(optional("public"), "type", $.identifier, $.type_descriptor, ";"),
+    type_descriptor: $ => choice(
+      $._basic_type),
+    _basic_type: $ => choice(
+      "int",
+      "float",
+      "string",
+      "boolean",
+      $._nil_literal
+    ),
+    _nil_literal: $ => choice("null", "()"),
+    word: $ => $.identifier,
+
+    identifier: $ => /[a-zA-Z_]\w*/, // This is strictly not correct according to spec but good enough for now
   }
 });
